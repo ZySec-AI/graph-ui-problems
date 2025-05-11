c104df3044dfb3639ad0131d086ee31d69bbe22b
AdityaTarale <tarale.adi@gmail.com>
2025-05-11 23:42:25 +0530

    Add: Info text component

---
+++ submissions/AdityaTarale/src/src/components/InfoText.tsx
@@ -0,0 +1,18 @@
+import React, { type FC } from "react";
+
+type InfoTextProps = {
+  type?: "default" | "error";
+  title: string;
+};
+
+const InfoText: FC<InfoTextProps> = ({ title, type = "default" }) => {
+  return (
+    <div
+      className={`flex flex-1 items-center justify-center text-sm text-${type === "default" ? "gray" : "red"}-500 sm:text-lg`}
+    >
+      {title}
+    </div>
+  );
+};
+
+export default InfoText;
