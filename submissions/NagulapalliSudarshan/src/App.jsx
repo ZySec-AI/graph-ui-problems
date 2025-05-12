4bab7c138cff482679928b41da799248a12d39b9
sudarshan <sudarshan.nagulapalli@gmail.com>
2025-05-12 18:11:04 +0530

    Ui changes

--- submissions/NagulapalliSudarshan/src/App.jsx
+++ submissions/NagulapalliSudarshan/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import GraphView from "./components/GraphView";
+import { ChartNetwork } from "lucide-react";
 
 function App() {
   const [graphData, setGraphData] = useState(null);
@@ -11,17 +12,21 @@ function App() {
       <Navbar />
       <div className="flex flex-1 overflow-hidden bg-gray-900">
         <Sidebar onDataLoad={setGraphData} />
-        <div className="flex-1 m-3 rounded-md bg-gray-800 overflow-hidden">
+        <div className="flex-1 m-3 rounded-md bg-gradient-to-r from-gray-800 to-slate-800 overflow-hidden border">
           {graphData ? (
             <GraphView data={graphData} />
           ) : (
             <div className="flex flex-col justify-center items-center h-full text-center">
-              <h2 className="text-3xl font-semibold text-gray-300">
-                Graph Viewer
+              <h2 className="text-3xl font-semibold text-gray-300 flex items-center gap-2">
+                <ChartNetwork size={40} className="text-blue-500" />
+                Graph Visualizer
               </h2>
-              <p className="text-gray-400 mt-2">
+              <span className="mt-4 text-white">
                 Upload or paste JSON to visualize the graph.
-              </p>
+              </span>
+              <span className="mt-2 text-gray-400 text-3xl">
+                ¯\_(ツ)_/¯
+              </span>
             </div>
           )}
         </div>
--- submissions/NagulapalliSudarshan/src/components/Navbar.jsx
+++ submissions/NagulapalliSudarshan/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { ChartNetwork } from "lucide-react";
 
 const Navbar = () => (
-  <div className="bg-gray-900 text-white p-4 shadow-md border-b border-gray-700 flex items-center justify-between">
+  <div className="bg-gray-900 text-white p-4 shadow-md border-b border-gray-700 flex items-center gap-2 bg-gradient-to-r from-gray-900 to-slate-800">
+    <ChartNetwork size={30  } className="text-blue-500" />
     <h1 className="text-2xl font-semibold">Graph Visualizer</h1>
   </div>
 );
