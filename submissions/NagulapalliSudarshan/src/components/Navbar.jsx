899f78861b77dc77db7da4f59c09da13508239ca
sudarshan <sudarshan.nagulapalli@gmail.com>
2025-05-19 19:23:23 +0530

    docs: update all dev and project details in readme

--- submissions/NagulapalliSudarshan/src/components/Navbar.jsx
+++ submissions/NagulapalliSudarshan/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { ChartNetwork, Menu, X } from "lucide-react";
  * This component renders a responsive navigation bar for the application.
  * 
  * Features:
- * - Displays the app logo (using ChartNetwork icon) and title "Graph Crafter"
+ * - Displays the app logo (using ChartNetwork icon) and title "JSON-2-Graph"
  * - Contains a toggle button that switches between open and close icons (Menu and X)
  * - The toggle button controls the visibility of a sidebar on smaller screens (responsive)
  * - Uses Tailwind CSS for styling and layout with a gradient background and shadow
@@ -20,7 +20,7 @@ const Navbar = ({ toggleSidebar, isSidebarOpen }) => (
   <div className="bg-gray-900 text-white p-4 shadow-md border-b border-gray-700 flex items-center justify-between gap-2 bg-gradient-to-r from-gray-900 to-slate-800">
     <div className="flex items-center gap-2">
       <ChartNetwork size={30} className="text-blue-500" />
-      <h1 className="text-2xl font-semibold">Graph Crafter</h1>
+      <h1 className="text-2xl font-semibold">JSON-2-Graph"</h1>
     </div>
     <button
       className="lg:hidden text-white focus:outline-none "
